a8e86e61d8e8c61010fa37f6f7d41b331e23bce3
Sander Jo <sander.jonkers+github@gmail.com>
2019-02-24 23:08:17 +0100

    Make it a module

--- speedometer.py
+++ speedometer.py
@@ -3,6 +3,8 @@
 import sys
 import time
 
+debug = False
+
 # Thinkbroad offers: 
 
 SizeUrlList = [
@@ -29,7 +31,7 @@ def measurespeed(url):
 
 	# Download the specified url, and report back MB/s and Mbps
 
-	print("URL is %s" % url)
+	if debug: print("URL is %s" % url)
 	start = time.time()
 	if sys.version_info[0] == 2:
 		import urllib2	# python2 only
@@ -43,7 +45,7 @@ def measurespeed(url):
 		print("ERROR: no python version?!")
 		return 0, 0
 
-	print("Downloaded bytes: %d" % downloadedbytes)
+	if debug: print("Downloaded bytes: %d" % downloadedbytes)
 
 	duration = time.time() - start
 
@@ -52,44 +54,59 @@ def measurespeed(url):
 	return MBps, mbps
 
 
+def BytestoBits(MBps):
+	return 8.05 * MBps # bits
+
+
+def runspeedometer():
+
+	# Do basic test
+
+	if debug: print("Basic measurement, with small download:")
+	urlbasic = SizeUrlList[0][1]
+	MBps, mbps = measurespeed(urlbasic)
+	if debug: print("Speed in MB/s: %.2f" % MBps)
+	if debug: print("Speed in Mbps: %.2f" % mbps)
+	maxMBps = MBps
+
+
+	'''
+	Based the first download, do a bigger download; the biggest download that still fits in 10 seconds
+	Rationale: a bigger download could yield higher MB/s because the 'starting delay' is relatively less
+	Calculation: 
+	If the 5MB download took 0.3 seconds, you can do a 30 times bigger download, so about 150 MB, will round to 100 MB
+	'''
+
+	if debug: print("\n\nMeasurement with bigger download, doable within 10 seconds")
+	maxtime = 10 # seconds
+
+	URLtoDO = None
+	for size,sizeurl in SizeUrlList:
+
+		expectedtime = size / MBps
+		#print(size,sizeurl, expectedtime)
+		if expectedtime < maxtime:
+			# ok, this one is feasible, so keep it in mind
+			URLtoDO = sizeurl
+
+	if URLtoDO:
+		if debug: print(URLtoDO)
+		MBps, mbps = measurespeed(URLtoDO)
+		if debug: print("Speed in MB/s: %.2f" % MBps)
+		if debug: print("Speed in Mbps: %.2f" % mbps)
+		if MBps > maxMBps:
+			maxMBps = MBps
+
+	return maxMBps
 
 ############### MAIN #######################
 
+if __name__ == '__main__':
+	print("Starting speed test:")
+	maxMBps = runspeedometer()
+	print("Speed in MB/s: %.2f" % maxMBps)
+	print("Speed in Mbps: %.2f" % BytestoBits(maxMBps))
 
-
-# Do basic test
-
-print("Basic measurement, with small download:")
-urlbasic = SizeUrlList[0][1]
-MBps, mbps = measurespeed(urlbasic)
-print("Speed in MB/s: %.2f" % MBps)
-print("Speed in Mbps: %.2f" % mbps)
-
-
-'''
-Based the first download, do a bigger download; the biggest download that still fits in 10 seconds
-Rationale: a bigger download could yield higher MB/s because the 'starting delay' is relatively less
-Calculation: 
-If the 5MB download took 0.3 seconds, you can do a 30 times bigger download, so about 150 MB, will round to 100 MB
-'''
-
-print("\n\nMeasurement with bigger download, doable within 10 seconds")
-maxtime = 10 # seconds
-
-URLtoDO = None
-for size,sizeurl in SizeUrlList:
-
-	expectedtime = size / MBps
-	#print(size,sizeurl, expectedtime)
-	if expectedtime < maxtime:
-		# ok, this one is feasible, so keep it in mind
-		URLtoDO = sizeurl
-
-if URLtoDO:
-	print(URLtoDO)
-	MBps, mbps = measurespeed(URLtoDO)
-	print("Speed in MB/s: %.2f" % MBps)
-	print("Speed in Mbps: %.2f" % mbps)
 
 
 
